9877ddbd4d0a8a765aeb9c357cbd15ebed7bf745
fleeyou <fleeyou@outlook.com>
2015-11-10 10:25:33 +0800

    add 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 5.cpp
+	> Author: Alan
+	> Mail: fleeyou@outlook.com
+	> Created Time: Mon 09 Nov 2015 08:03:56 PM CST
+    > Problem Name: Longest Palindromic Substring My Submissions Question
+    > Difficulty: Medium
+    > Description:
+        Given a string S, find the longest palindromic substring in S. You may assume that the maximum 
+        length of S is 1000, and there exists one unique longest palindromic substring.
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
