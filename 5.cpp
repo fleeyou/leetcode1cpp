4dc8a88b5568da6162bcc066f058ed4f2a5c6b18
fleeyou <fleeyou@outlook.com>
2015-11-10 10:26:17 +0800

    add 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -11,5 +11,35 @@
  ************************************************************************/
 
 #include<iostream>
+#include<cstring>
+
 using namespace std;
 
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        const int len=s.size();
+        bool p[len][len];
+        memset(p,false,len*len);
+        int max_len=1, start=0;
+        for(int j = 0; j<len;j++){
+            p[j][j]=true;
+            for(int i = 0; i<j;i++){
+                p[i][j]=(s[i]==s[j]&&(j-i<2||p[i+1][j-1]));
+                if(p[i][j]&&max_len<(j-i+1)){
+                    max_len=j-i+1;
+                    start=i;
+                }
+            }
+        }
+        return s.substr(start, max_len);
+    }
+};
+
+int main(){
+    string s{
+        "abcbacd"
+    };
+    Solution sol = Solution();
+    cout<<"The longest palindromic substring in the string is: "<<sol.longestPalindrome(s)<<endl;
+}
